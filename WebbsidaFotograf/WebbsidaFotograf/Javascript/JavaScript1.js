4202afbccbcb56bf4065d56de56d403a7071c907
me222rs <me222rs@student.lnu.se>
2014-06-08 12:57:17 +0200

    kommentering
    har skrivit lite kommentarer om vart jag har tagit koden ifrån

--- WebbsidaFotograf/WebbsidaFotograf/Javascript/JavaScript1.js
+++ WebbsidaFotograf/WebbsidaFotograf/Javascript/JavaScript1.js
@@ -1,4 +1,6 @@
-﻿(function(d, s, id) {
+﻿
+//Dessa två funktioner har jag tagit från facebook
+(function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
@@ -14,7 +16,7 @@
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-
+//Twitter har jag tagit denna koden ifrån
 !function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'twitter-wjs');
 
 
